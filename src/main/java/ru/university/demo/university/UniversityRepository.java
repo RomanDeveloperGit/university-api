f6dc828291c0e5e4e3e690e380c2596bfdb466be
roman_developer <89508006+RomanDeveloperGit@users.noreply.github.com>
2023-11-21 13:38:55 +0300

    feat(services): add university service interface

--- src/main/java/ru/university/demo/university/UniversityRepository.java
+++ src/main/java/ru/university/demo/university/UniversityRepository.java
@@ -2,4 +2,4 @@ package ru.university.demo.university;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UniversityRepository extends JpaRepository<UniversityModel, Long> {}
+public interface UniversityRepository extends JpaRepository<UniversityModel, Long> { }
--- src/main/java/ru/university/demo/university/UniversityService.java
+++ src/main/java/ru/university/demo/university/UniversityService.java
@@ -1,13 +1,16 @@
 package ru.university.demo.university;
 
-import java.time.LocalDate;
+import ru.university.demo.university.dto.CreateUniversityDTO;
+import ru.university.demo.university.dto.UpdateUniversityDTO;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface UniversityService {
   // todo: pagination
   List<UniversityModel> getUniversityList();
-  UniversityModel getUniversity(Long id);
-  UniversityModel createUniversity(String name, LocalDate foundationDate);
-  UniversityModel updateUniversityName(String newName);
+  Optional<UniversityModel> getUniversity(Long id);
+  UniversityModel createUniversity(CreateUniversityDTO dto);
+  UniversityModel updateUniversity(Long id, UpdateUniversityDTO dto);
   void deleteUniversity(Long id);
 }
