67c6fbd2baee4d5d55b4850e62cdf29f246eeac6
roman_developer <89508006+RomanDeveloperGit@users.noreply.github.com>
2023-11-21 12:23:42 +0300

    feat(repositories): add university repository

---
+++ src/main/java/ru/university/demo/university/UniversityRepository.java
@@ -0,0 +1,5 @@
+package ru.university.demo.university;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UniversityRepository extends JpaRepository<UniversityModel, Long> {}
